28561451b60e9d6c2bc348d760d52986b708b658
shota-suzuki <98932699+shotabeja@users.noreply.github.com>
2022-06-28 10:27:08 +0900

    Add config for continuous integration (#3)
    add config for continuous integration

--- exercises/01-fastapi-sample/sql_app/main.py
+++ exercises/01-fastapi-sample/sql_app/main.py
@@ -20,13 +20,16 @@ def get_db():
         db.close()
 
 
+db_session = Depends(get_db)
+
+
 @app.get("/health-check")
-def health_check(db: Session = Depends(get_db)):
+def health_check(db: Session = db_session):
     return {"status": "ok"}
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = db_session):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -34,13 +37,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = db_session):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = db_session):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -49,12 +52,12 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
 def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+    user_id: int, item: schemas.ItemCreate, db: Session = db_session
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
 @app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(skip: int = 0, limit: int = 100, db: Session = db_session):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
